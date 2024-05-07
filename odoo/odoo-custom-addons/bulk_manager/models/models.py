fbafb546c70b25d175f9ec1fb9fd3147c77a02f4
tseOrlando <2023_tese.araujo@iticbcn.cat>
2024-05-07 17:28:50 +0200

    advacend bulk manager, changed design and fields

--- odoo/odoo-custom-addons/bulk_manager/models/models.py
+++ odoo/odoo-custom-addons/bulk_manager/models/models.py
@@ -1,18 +1,19 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo import api, exceptions, fields, models, SUPERUSER_ID
 
+class bulk_manager_groups(models.Model):
+    _name = 'bulk_manager.bulk_manager_groups'
+    _description = 'bulk_manager.bulk_manager_groups'
 
+    name = fields.Char(string='Name', required=True)
+    
 class bulk_manager(models.Model):
     _name = 'bulk_manager.bulk_manager'
     _description = 'bulk_manager.bulk_manager'
 
-    name = fields.Char()
-    value = fields.Integer()
-    value2 = fields.Float(compute="_value_pc", store=True)
-    description = fields.Text()
-
-    @api.depends('value')
-    def _value_pc(self):
-        for record in self:
-            record.value2 = float(record.value) / 100
+    name = fields.Char(string='Name')
+    quantity = fields.Integer(string='Quantity', required=True)
+    price = fields.Monetary(string='Price', currency='EUR')
+    group = fields.Many2one('bulk_manager.bulk_manager_groups', string='Group')
