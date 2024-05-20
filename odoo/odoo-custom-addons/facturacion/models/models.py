8bd78b8ee48ae83468c24fc72a6101edeb7ccf16
RamonFont <2023_ramon.fontiticbcn.cat>
2024-05-20 21:35:14 +0200

    Modulo facturacion Pagina inicial

--- odoo/odoo-custom-addons/facturacion/models/models.py
+++ odoo/odoo-custom-addons/facturacion/models/models.py
@@ -1,18 +1,17 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
+class FacturacionVentaEntradas(models.Model):
+    _name = 'facturacion_venta_entradas.facturacion_venta_entradas'
+    _description = 'Facturacion Venta Entradas'
 
-# class facturacion(models.Model):
-#     _name = 'facturacion.facturacion'
-#     _description = 'facturacion.facturacion'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    factura_numero = fields.Char(string="Número de Factura")
+    cliente_id = fields.Many2one('res.partner', string="Cliente")
+    fecha_factura = fields.Date(string="Fecha de Factura")
+    total_euros = fields.Float(string="Total en Euros")
+    estado = fields.Selection([
+        ('borrador', 'Borrador'),
+        ('pagada', 'Pagada'),
+        ('cancelada', 'Cancelada')
+    ], string="Estado", default='borrador')
