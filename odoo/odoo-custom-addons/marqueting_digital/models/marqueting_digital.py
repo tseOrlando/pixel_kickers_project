9972d268bb9a5e3c8807a306a4e04218efd22a56
RamonFont <2023_ramon.fontiticbcn.cat>
2024-05-20 19:44:33 +0200

    Modul Marqueting per email

--- odoo/odoo-custom-addons/marqueting_digital/models/marqueting_digital.py
+++ odoo/odoo-custom-addons/marqueting_digital/models/marqueting_digital.py
@@ -1,15 +1,16 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from datetime import datetime
 
 class MarquetingDigital(models.Model):
     _name = 'marqueting_digital.marqueting_digital'
     _description = 'Marqueting Digital'
 
-    name = fields.Date(string='Date', default=fields.Date.today)
     value = fields.Char(string='Subject')
-    value2 = fields.Char(string='Submiter')
-    value3 = fields.Char(string='Recipient')
+    name = fields.Date(string='Date', default=fields.Date.today)
+    value2 = fields.Char(string='Submitter', default="odoo13")
+    value3 = fields.Many2one('marqueting_digital.mailing_list', string='Recipient')
     description = fields.Text(string='Description')
 
 
@@ -17,13 +18,24 @@ class Email(models.Model):
     _name = 'marqueting_digital.email'
     _description = 'Email'
 
-    date = fields.Datetime(string='Date')
     subject = fields.Char(string='Subject')
-    sender = fields.Char(string='Sender')
     content = fields.Text(string='Content')
-    recipient = fields.Char(string='Recipient')
-    marqueting_digital_id = fields.Many2one('marqueting_digital.marqueting_digital', string='Marqueting Digital')
+    recipient = fields.Text(string='Recipient')  
     mailing_list_id = fields.Many2one('marqueting_digital.mailing_list', string='Mailing List')
+    sender = fields.Char(string='Sender', default='odoo13')  
+
+    date = fields.Date(string='Date')
+
+    @api.model
+    def create(self, vals):
+        vals['sender'] = 'odoo13' 
+        vals['date'] = fields.Date.today()
+        return super(Email, self).create(vals)
+
+    @api.onchange('mailing_list_id')
+    def _onchange_mailing_list(self):
+        if self.mailing_list_id:
+            self.recipient = self.mailing_list_id
 
 
 class MailingList(models.Model):
@@ -33,7 +45,6 @@ class MailingList(models.Model):
     name = fields.Char(string='Name')
     email_count = fields.Integer(string='Email Count', compute='_compute_email_count')
     emails = fields.One2many('marqueting_digital.email', 'mailing_list_id', string='Emails')
-    marqueting_digital_id = fields.Many2one('marqueting_digital.marqueting_digital', string='Marqueting Digital')
 
     @api.depends('emails')
     def _compute_email_count(self):
