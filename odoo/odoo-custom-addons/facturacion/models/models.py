1497fae057001cba537a4a02b585d69d2374c402
RamonFont <2023_ramon.fontiticbcn.cat>
2024-05-20 23:05:08 +0200

    Modulo facturacion creacion cliente

--- odoo/odoo-custom-addons/facturacion/models/models.py
+++ odoo/odoo-custom-addons/facturacion/models/models.py
@@ -1,17 +1,27 @@
 # -*- coding: utf-8 -*-
-
 from odoo import models, fields, api
 
-class FacturacionVentaEntradas(models.Model):
-    _name = 'facturacion_venta_entradas.facturacion_venta_entradas'
-    _description = 'Facturacion Venta Entradas'
+class VentasFacturas(models.Model):
+    _name = 'ventas_facturas.ventas_facturas'
+    _description = 'Facturas de Ventas'
 
-    factura_numero = fields.Char(string="Número de Factura")
-    cliente_id = fields.Many2one('res.partner', string="Cliente")
-    fecha_factura = fields.Date(string="Fecha de Factura")
-    total_euros = fields.Float(string="Total en Euros")
-    estado = fields.Selection([
-        ('borrador', 'Borrador'),
-        ('pagada', 'Pagada'),
-        ('cancelada', 'Cancelada')
-    ], string="Estado", default='borrador')
+    numero_factura = fields.Char(string="Invoice Number", required=True)
+    cliente = fields.Many2one('res.partner', string="Customer", required=True)
+    fecha_factura = fields.Date(string="Date", required=True)
+    precio = fields.Float(string="Price", required=True)
+    estado_factura = fields.Selection([
+        ('draft', 'Draft'),
+        ('confirmed', 'Confirmed'),
+        ('paid', 'Paid'),
+        ('canceled', 'Canceled')
+    ], string="Status", required=True, default='draft')
+
+class Cliente(models.Model):
+    _name = 'clientes.cliente'
+    _description = 'Cliente'
+
+    name = fields.Char(string='Name', required=True)
+    last_name = fields.Char(string='Last Name')
+    company_name = fields.Char(string='Nombre Emp')
+    mobile = fields.Char(string='Phone')
+    category_id = fields.Many2one('res.partner.category', string='Categoría')
