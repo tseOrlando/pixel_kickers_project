12d116bfebb310d5fda61f3da3b81e993db11416
RamonFont <2023_ramon.fontiticbcn.cat>
2024-05-20 18:20:10 +0200

    Creacio List Modulo marqueting_digital

--- odoo/odoo-custom-addons/marqueting_digital/models/marqueting_digital.py
+++ odoo/odoo-custom-addons/marqueting_digital/models/marqueting_digital.py
@@ -6,20 +6,15 @@ class MarquetingDigital(models.Model):
     _name = 'marqueting_digital.marqueting_digital'
     _description = 'Marqueting Digital'
 
-    name = fields.Char(string='Holiwiiwiwiwiwaa')
-    value = fields.Integer(string='Value')
-    value2 = fields.Float(string='Value 2', compute="_value_pc", store=True)
+    name = fields.Date(string='Date', default=fields.Date.today)
+    value = fields.Char(string='Subject')
+    value2 = fields.Char(string='Submiter')
+    value3 = fields.Char(string='Recipient')
     description = fields.Text(string='Description')
 
-    email_ids = fields.One2many(comodel_name='marqueting_digital.email', inverse_name='marqueting_digital_id', string='Emails')
-
-    @api.depends('value')
-    def _value_pc(self):
-        for record in self:
-            record.value2 = float(record.value) / 100
 
 class Email(models.Model):
-    _name = 'marqueting_digital.emaail'
+    _name = 'marqueting_digital.email'
     _description = 'Email'
 
     date = fields.Datetime(string='Date')
@@ -28,3 +23,19 @@ class Email(models.Model):
     content = fields.Text(string='Content')
     recipient = fields.Char(string='Recipient')
     marqueting_digital_id = fields.Many2one('marqueting_digital.marqueting_digital', string='Marqueting Digital')
+    mailing_list_id = fields.Many2one('marqueting_digital.mailing_list', string='Mailing List')
+
+
+class MailingList(models.Model):
+    _name = 'marqueting_digital.mailing_list'
+    _description = 'Mailing List'
+
+    name = fields.Char(string='Name')
+    email_count = fields.Integer(string='Email Count', compute='_compute_email_count')
+    emails = fields.One2many('marqueting_digital.email', 'mailing_list_id', string='Emails')
+    marqueting_digital_id = fields.Many2one('marqueting_digital.marqueting_digital', string='Marqueting Digital')
+
+    @api.depends('emails')
+    def _compute_email_count(self):
+        for record in self:
+            record.email_count = len(record.emails)
