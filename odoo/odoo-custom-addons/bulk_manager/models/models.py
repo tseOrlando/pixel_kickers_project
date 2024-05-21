09bd2e1499bb594f6484a8eb9c40e3fd36380cec
tseOrlando <2023_tese.araujo@iticbcn.cat>
2024-05-21 13:22:40 +0200

    bulk manager created successfully with its item inside

--- odoo/odoo-custom-addons/bulk_manager/models/models.py
+++ odoo/odoo-custom-addons/bulk_manager/models/models.py
@@ -1,20 +1,23 @@
-# -*- coding: utf-8 -*-
-
 from odoo import models, fields
-from odoo import api, fields, models, SUPERUSER_ID
 
-class bulk_manager_groups(models.Model):
+class BulkManagerGroups(models.Model):
     _name = 'bulk_manager.bulk_manager_groups'
     _description = 'bulk_manager.bulk_manager_groups'
 
     name = fields.Char(string='Name', required=True)
-    
-class bulk_manager(models.Model):
-    _name = 'bulk_manager.bulk_manager'
-    _description = 'bulk_manager.bulk_manager'
+    product_ids = fields.One2many('bulk_manager.bulk_manager_product', 'bulk_group_id', string='Products')
 
-    name = fields.Char(string='Name')
+class BulkManagerProduct(models.Model):
+    _name = 'bulk_manager.bulk_manager_product'
+    _description = 'bulk_manager.bulk_manager_product'
+
+    name = fields.Char(string='Name', required=True)
     quantity = fields.Integer(string='Quantity', required=True)
-    price = fields.Integer(string='Price', currency_field='currency_id', required=True)
-    currency_id = fields.Many2one('res.currency', string='Currency', default=lambda self: self.env.ref('base.EUR').id)
-    bulk_group = fields.Many2one('bulk_manager.bulk_manager_groups', string='Bulk Group')
+    price = fields.Float(string='Price', required=True)
+    currency_id = fields.Many2one(
+        'res.currency', string='Currency',
+        default=lambda self: self.env.ref('base.EUR').id
+    )
+    bulk_group_id = fields.Many2one(
+        'bulk_manager.bulk_manager_groups', string='Bulk Group'
+    )
